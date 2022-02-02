d666bf1fa1ac6129e4048c0cc82531ed1c4cd3bd
enocisai <enocisaicastro@gmail.com>
2022-02-02 10:36:50 -0700

    colors and keyboard fuctions added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-function incrementButton(){
+function incrementButton() {
+
     let element = document.getElementById('number');
     let value = element.innerHTML;
 
+
     ++value;
-    
-    document.getElementById('number').innerHTML = value;
+    document.getElementById("number").innerHTML = value;
+    console.log("value coming from incrementButton() = ", value)
+    colorChanger(value)
+
+
 }
 
-function decreaseButton(){
+function decreaseButton() {
     let element = document.getElementById("number");
     let value = element.innerHTML;
 
@@ -15,8 +20,40 @@ function decreaseButton(){
     document.getElementById("number").innerHTML = value;
 }
 
-function pauseButton(){
+function pauseButton() {
     clearInterval(numberUp)
 
 }
+
+//Keyboard control
+document.addEventListener("keydown", (e) => {
+
+
+    const moveForwardKeys = ["ArrowUp", "ArrowRight"]
+    const moveBackwards = ["ArrowLeft", "ArrowDown"]
+    if (moveForwardKeys.includes(e.key)) {
+        e.preventDefault()
+        incrementButton()
+    }
+    if (moveBackwards.includes(e.key)) {
+        e.preventDefault()
+        decreaseButton()
+    }
+
+})
+
+
+//Background Color Change
+
+
+function colorChanger(positionOfColorToUse) {
+    let colors = ["green", "red", "yellow", "purple", "pink", "blue","brown", "gray", "orange", ];
+    const nextPosition = (positionOfColorToUse - 1) % colors.length
+    document.body.style.backgroundColor = colors[nextPosition];
+
+}
+
+
+
+
 
